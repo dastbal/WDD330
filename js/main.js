f403c16beae87c19118f420a792960f332a4815f
dastballadares <bal20001@byui.edu>
2021-04-29 15:53:36 -0500

    week 2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const links =[
         label :" Week 1 notes",
         url : "./week1/index.html"
     },
+    {
+        label :" Week 2 notes",
+        url : "./week2/index.html"
+    },
 ]
 
 
