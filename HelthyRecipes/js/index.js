d051b9ee342a6ec9a71bba7ef1064b9a729cbf3f
dastballadares <bal20001@byui.edu>
2021-06-29 22:22:13 -0500

    api working

--- HelthyRecipes/js/index.js
+++ HelthyRecipes/js/index.js
@@ -1,20 +1,16 @@
 const API = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=74fbd4893ddf42fb986637f39f01dc28&query=';
 
 const root = document.getElementById('container')
+const containerSearch = document.getElementById('containerSearch')
 
 
 const showSearchInput = ()=>{
-    const searchInput = document.createElement('div');
+    const searchInput = document.createElement('form');
     searchInput.id='search';
     searchInput.innerHTML =`
-    <div class="search--header">
-    <p>Search</p>
-    <button><i class="fas fa-search"></i>
-    
-    </button>
-    </div>
-    <input placeholder="Search" id="searchInput" type="text" >`
-    root.append(searchInput)
+    <button  class="search--header"> Search   <i class="fas fa-search"></i></button>    
+        <input placeholder="Search" id="searchInput" type="text" >`
+    containerSearch.append(searchInput);
     
 }
 window.addEventListener('load',showSearchInput)
@@ -41,11 +37,13 @@ async function getRecipeSearch(){
 
 
 //  this function will render  the html of the serac recipes
-async function showSearchRecipes(){
+function showSearchRecipes(recipesInfo){
+    root.innerHTML= '<h3> Loading... </h3>'
+    root.innerHTML = ''
     const recipes = document.createElement('div')
     recipes.classList.add('recipes')
-    const json = await getRecipeSearch('chocolate');
-    json.results.forEach(element => {
+    const info = recipesInfo;
+    info.results.forEach(element => {
         const { id, title, image} = element;
         const recipe = document.createElement('div')
         recipe.classList.add('recipe')
@@ -66,5 +64,15 @@ async function showSearchRecipes(){
 
 
 }
+async function searchRecipes(e){
+    e.preventDefault();
+    const dataInfo = await getRecipeSearch();
+    console.log(data)
+    showSearchRecipes(dataInfo);
+    
 
-showSearchRecipes()
+}
+
+let form = document.getElementById('search');
+form.addEventListener('submit', e =>{searchRecipes(e)});
+
